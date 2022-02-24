993ed6c5a1835cd1052fa310d5d9f99ed5c3d1b3
nasirdiu <nasir.joypurhat@gmail.com>
2022-02-24 19:03:11 +0600

    card

--- js/card.js
+++ js/card.js
@@ -3,26 +3,25 @@ const buttonSearch = () => {
   const error = document.getElementById("error");
   const carText = parseInt(carInput.value);
   const main = document.getElementById("main");
-  main.innerHTML='';
+  main.innerHTML = "";
   carInput.value = "";
   if (isNaN(carText) || carText == "") {
     error.innerText = "this is a Number";
   } else if (carText <= 0) {
     error.innerText = "this a Negative number";
   } else {
-    main.innerHTML='';
+    main.innerHTML = "";
     fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${carText}`)
       .then((res) => res.json())
       .then((data) => displayCard(data.cards));
-      error.innerHTML='';
+    error.innerHTML = "";
   }
 };
 
 const displayCard = (card) => {
- 
   card.forEach((car) => {
     const div = document.createElement("div");
-    div.classList.add("card")
+    div.classList.add("card");
     div.innerHTML = `<div class="card" style="width: 18rem;">
         <img src="${car.image}" class="card-img-top" alt="...">
         <div class="card-body">
@@ -36,13 +35,25 @@ const displayCard = (card) => {
   });
 };
 
-const search=cards=>{
-    fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=52`)
+const search = (code) => {
+  fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=52`)
     .then((res) => res.json())
     .then((data) => {
-        const allCard=data.cards;
-        const shignal=allCard.find(card=>card.cards ===cards)
-        console.log(shignal);
+      const allCard = data.cards;
+      const singleCard = allCard.find((card) => card.code === code);
+      const div = document.createElement("div");
+      main.innerHTML='';
+      div.innerHTML = `
+      <div class="card" style="width: 18rem;">
+        <img src="${singleCard.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${singleCard.suit}</h5>
+          <p class="card-text">Value --${singleCard.value}</p>
+          <p class="card-text">${singleCard.code}</p>
 
+        </div>
+      </div>
+      `;
+      main.appendChild(div);
     });
-}
+};
